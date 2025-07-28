6765a114626f3cc8eb334050ad1fd7f6608d1fdd
root <root@ip-10-120-0-41.ap-south-1.compute.internal>
2025-07-28 10:28:01 +0000

    Fix Lambda environment variable configuration for deployment

--- app/amplify/backend.ts
+++ app/amplify/backend.ts
@@ -100,11 +100,8 @@ backend.data.resources.tables["GoldStandardEssay"].grantReadData(
   backend.processEssay.resources.lambda
 );
 
-// Add table names as environment variables to processEssay Lambda
-backend.processEssay.resources.lambda.addEnvironment('ESSAY_TABLE_NAME', backend.data.resources.tables["Essay"].tableName);
-backend.processEssay.resources.lambda.addEnvironment('RESULT_TABLE_NAME', backend.data.resources.tables["Result"].tableName);
-backend.processEssay.resources.lambda.addEnvironment('USER_TABLE_NAME', backend.data.resources.tables["User"].tableName);
-backend.processEssay.resources.lambda.addEnvironment('GOLD_STANDARD_TABLE_NAME', backend.data.resources.tables["GoldStandardEssay"].tableName);
+// Table names are configured in the resource.ts files
+// They will be set dynamically during deployment
 
 // Grant read/write access to GoldStandardEssay table for generateEmbeddings
 backend.data.resources.tables["GoldStandardEssay"].grantReadWriteData(
@@ -223,8 +220,8 @@ essayQueue.grantConsumeMessages(backend.processEssay.resources.lambda);
 essayQueue.grantSendMessages(backend.submitEssayToQueue.resources.lambda);
 dlq.grantConsumeMessages(backend.processEssay.resources.lambda); // Allow Lambda to read from DLQ
 
-// Add the queue URL as an environment variable to submitEssayToQueue Lambda
-backend.submitEssayToQueue.resources.lambda.addEnvironment('ESSAY_QUEUE_URL', essayQueue.queueUrl);
+// Queue URL is configured in the resource.ts file
+// It will be set dynamically during deployment
 
 // Create SNS topic for DLQ alerts
 const dlqAlertTopic = new Topic(dataStack, 'DLQAlertTopic', {
@@ -271,16 +268,16 @@ backend.data.resources.tables["Essay"].grantReadWriteData(
   backend.submitEssayToQueue.resources.lambda
 );
 
-// Add the table name as an environment variable to submitEssayToQueue Lambda
-backend.submitEssayToQueue.resources.lambda.addEnvironment('ESSAY_TABLE_NAME', backend.data.resources.tables["Essay"].tableName);
+// Table name is configured in the resource.ts file
+// It will be set dynamically during deployment
 
 // Grant generateEmbeddings function access to GoldStandardEssay table
 backend.data.resources.tables["GoldStandardEssay"].grantReadWriteData(
   backend.generateEmbeddings.resources.lambda
 );
 
-// Add table name as environment variable to generateEmbeddings Lambda
-backend.generateEmbeddings.resources.lambda.addEnvironment('GOLD_STANDARD_TABLE_NAME', backend.data.resources.tables["GoldStandardEssay"].tableName);
+// Table name is configured in the resource.ts file
+// It will be set dynamically during deployment
 
 // Grant generateEmbeddings permission to use Bedrock
 backend.generateEmbeddings.resources.lambda.addToRolePolicy(
